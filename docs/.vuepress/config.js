7060bb372a02f1a13635721e6f47f31c8d4cd451
xuchuguo <xuchuguo@qq.com>
2020-08-13 17:00:54 +0800

    修改描述

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "title": "许楚国的博客",
-  "description": "做一个管理时间的人",
+  "description": "勤能补拙",
   // "dest": "public", // build后的输出文件夹,默认  .vuepress/dist
   "base": '/my-blog/',
   "head": [
